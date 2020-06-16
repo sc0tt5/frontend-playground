729e0c9ec5835d00d3ce9d7062f7a0a628f060d3
Scott Robertson <contact@ScottRobertson.io>
2020-06-15 22:16:23 -0500

    creation operators - from

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,16 @@
-import { Observable } from 'rxjs';
+import { from } from 'rxjs';
+
+function* hello() {
+  yield 'Hello';
+  yield 'World';
+}
 
 const observer = {
-  next: value => console.log('next', value),
-  error: error => console.log('error', error),
-  complete: () => console.log('complete')
+  next: value => console.log('next:::', value),
+  error: error => console.log('error:::', error),
+  complete: () => console.log('complete!')
 };
 
-const observable = new Observable(subscriber => {
-  let count = 0;
+const source$ = from(fetch('https://api.github.com/users/octocat'));
 
-  const id = setInterval(() => {
-    subscriber.next(count);
-    count += 1;
-  }, 1000);
-
-  return () => {
-    console.log('called');
-    clearInterval(id);
-  };
-});
-
-const subscription = observable.subscribe(observer);
-
-setTimeout(() => {
-  subscription.unsubscribe();
-}, 3500);
+source$.subscribe(observer);
