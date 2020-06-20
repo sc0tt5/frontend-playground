0b5d7e30163093ea5b0ea4225b1020eb1e5da879
Scott Robertson <contact@ScottRobertson.io>
2020-06-20 14:25:27 -0500

    complete stream based on another stream  using takeUntil

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
-import { interval } from 'rxjs';
-import { mapTo, scan, takeWhile, tap } from 'rxjs/operators';
+import { fromEvent, interval } from 'rxjs';
+import { mapTo, scan, takeUntil, takeWhile, tap } from 'rxjs/operators';
 
 // elem refs
 const countdown = document.getElementById('countdown');
 const message = document.getElementById('message');
+const abortButton = document.getElementById('abort');
 
 // streams
 const counter$ = interval(1000);
+const abortClick$ = fromEvent(abortButton, 'click');
 
 counter$
   .pipe(
     mapTo(-1),
     scan((accumulator, current) => accumulator + current, 10),
     tap(console.log), // takeWhile will prevent from continuing when 0
-    takeWhile(value => value >= 0)
+    takeWhile(value => value >= 0),
+    takeUntil(abortClick$) // the event will stop the timer
   )
   .subscribe(value => {
     countdown.innerHTML = value.toString();
