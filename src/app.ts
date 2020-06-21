7d4902f9be91baa39b94b0ae2506cd3906daabb4
Scott Robertson <contact@ScottRobertson.io>
2020-06-20 21:13:37 -0500

    rate limiting operators - throttleTime

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import { fromEvent } from 'rxjs';
-import { debounceTime, distinctUntilChanged, pluck } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, pluck, throttleTime } from 'rxjs/operators';
 
 // elem refs
 const inputBox = document.getElementById('text-input');
 
 // streams
 const click$ = fromEvent(document, 'click');
-const input$ = fromEvent(inputBox, 'keyup');
 
-input$
-  .pipe(debounceTime(1000), pluck('target', 'value'), distinctUntilChanged())
-  .subscribe(console.log);
+click$.pipe(throttleTime(3000)).subscribe(console.log);
