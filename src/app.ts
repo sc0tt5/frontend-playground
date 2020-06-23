b4a2d4b258c54218c996d2f1575cc404e3193ac4
Scott Robertson <contact@ScottRobertson.io>
2020-06-22 22:15:42 -0500

    lab 3 - create an http polling solution

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,40 @@
-import { EMPTY, fromEvent } from 'rxjs';
+import { fromEvent, timer } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import { catchError, debounceTime, distinctUntilChanged, pluck, switchMap } from 'rxjs/operators';
+import { exhaustMap, finalize, pluck, switchMapTo, takeUntil, tap } from 'rxjs/operators';
 
-/** note: use mergeMap for post/save and switchMap when
- * call is cancellable like get/read
- *
- */
-const BASE_URL = 'https://api.openbrewerydb.org/breweries';
 // elem refs
-const inputBox = document.getElementById('text-input');
-const typeaheadContainer = document.getElementById('typeahead-container');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const pollingStatus = document.getElementById('polling-status');
+const dogImage: any = document.getElementById('dog');
 
 // streams
-const input$ = fromEvent(inputBox, 'keyup');
+const startClick$ = fromEvent(startButton, 'click');
+const stopClick$ = fromEvent(stopButton, 'click');
 
-input$
+startClick$
   .pipe(
-    debounceTime(200),
-    pluck('target', 'value'),
-    distinctUntilChanged(),
-    switchMap(searchTerm => {
-      return ajax.getJSON(`${BASE_URL}?by_name=${searchTerm}`).pipe(catchError(error => EMPTY));
-    })
+    /*
+     * Every start click we will map to an interval which
+     * emits every 5 seconds to request a new image.
+     * Since we do not want multiple polls active at once,
+     * we'll use exhaustMap to ignore any emissions
+     * while the inner interval is running.
+     */
+    exhaustMap(() =>
+      timer(0, 5000).pipe(
+        tap(() => (pollingStatus.innerHTML = 'Active')),
+        switchMapTo(ajax.getJSON('https://random.dog/woof.json').pipe(pluck('url'))),
+        /*
+         * Cancel the poll when stop click stream emits
+         */
+        takeUntil(stopClick$),
+        /*
+         * We'll use finalize to update the status to stopped
+         * each time the inner observable completes.
+         */
+        finalize(() => (pollingStatus.innerHTML = 'Stopped'))
+      )
+    )
   )
-  .subscribe((response: any[]) => {
-    typeaheadContainer.innerHTML = response.map(b => b.name).join('<br>');
-  });
+  .subscribe(url => (dogImage.src = url));
